4a9c43f70cd2446d177b25eb9d1941e13a0d5e5b
Kyohei-us <kyohei1212000@gmail.com>
2019-10-21 01:24:07 -0700

    'twitterL mod'

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ async def on_reaction_add(reaction, user):
 #         continue
 
 @bot.command(name='twitterL')
-async def fetch_twitter_list(ctx, *, arg):
+async def fetch_twitter_list(ctx, *args):
     channel = ctx.message.channel
     tweets = []
-    arg = int(arg)
-    tweets = tw.seeLists(tweets = tweets, count = arg)
+    count = int(args[0])
+    tweets = tw.seeLists(tweets = tweets, count = count)
     for tweet in tweets:
         await channel.send(tweet)
     await channel.send("Twitter List is all!")
@@ -63,21 +63,21 @@ async def greet_my_bot(ctx, *, arg):
     else:
         await channel.send("Do you not wanna greet me? Come on! lol")
 
-@bot.command(name='twitterS')
-async def search_on_twitter(ctx, *, arg):
-    channel = ctx.message.channel
-    message = ctx.message
-    extractCountList = message.content.split(',')
-    extractKeywordList = extractCountList[0].split(' ')
-    tweets = []
-    if len(extractCountList) > 1:
-        tweets = tw.letscrawl(tweets, message.content, count = int(extractCountList[1]))
-    else:
-        tweets = tw.letscrawl(tweets, message.content)
-
-    for tweet in tweets:
-        await channel.send(tweet)
-    await channel.send("Twitter Search Finished!")
+# @bot.command(name='twitterS')
+# async def search_on_twitter(ctx, *, arg):
+#     channel = ctx.message.channel
+#     message = ctx.message
+#     extractCountList = message.content.split(',')
+#     extractKeywordList = extractCountList[0].split(' ')
+#     tweets = []
+#     if len(extractCountList) > 1:
+#         tweets = tw.letscrawl(tweets, message.content, count = int(extractCountList[1]))
+#     else:
+#         tweets = tw.letscrawl(tweets, message.content)
+#
+#     for tweet in tweets:
+#         await channel.send(tweet)
+#     await channel.send("Twitter Search Finished!")
 
 @bot.event
 async def on_message(message):
