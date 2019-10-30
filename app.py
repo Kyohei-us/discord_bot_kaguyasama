a3f64593de4443e372c83e27f7eaee91436615cf
Kyohei <kyohei1212000@gmail.com>
2019-10-30 01:33:31 -0700

    'ok'

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ async def on_message_delete(message):
     name_of_channel_message_was_in = message.channel.name
 
     try:
-        imageAttached = message.attachment.url
+        imageAttached = message.attachment[0].url
     except Exception as e:
         print(e)
         await channel.send(imageAttached)
